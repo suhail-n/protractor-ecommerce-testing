5d56674fd5e21dde9b0e3b52c8ee40d6346deb12
Suhail Nooristani <20075736+suhail-n@users.noreply.github.com>
2019-11-03 22:21:24 -0500

    Removed dist folder

--- dist/common/locator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getLocatorProperties(name, locator, locatorType, description) {
-    let props = {
-        name, locator, locatorType, description
-    };
-    return props;
-}
-exports.getLocatorProperties = getLocatorProperties;
